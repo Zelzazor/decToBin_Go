3e3fca76b85db3e2f73c81b562c92e3952f8f523
Felipe Ramírez <zelzazor.ramirez62@gmail.com>
2021-06-06 20:45:29 -0400

    Cambios a la implementación de decToBin: Removida función auxiliar ReverseString

--- decToBin.go
+++ decToBin.go
@@ -5,14 +5,6 @@ import (
 	"strconv"
 )
 
-func reverseString(str string) string {
-	strArr := []rune(str)
-	for i, j := 0, len(strArr)-1; i < j; i, j = i+1, j-1 {
-		strArr[i], strArr[j] = strArr[j], strArr[i]
-	}
-	return string(strArr)
-}
-
 func decToBin(number int) (string, error) {
 	if number < 0 {
 		return "0", errors.New("number is out of range")
@@ -20,11 +12,13 @@ func decToBin(number int) (string, error) {
 		return "0", nil
 	} else {
 		var result = ""
+		var char = ""
 		for number > 0 {
-			result += strconv.Itoa(number % 2)
+			char = strconv.Itoa(number % 2)
+			result = char + result
 			number /= 2
 		}
-		result = reverseString(result)
+
 		return result, nil
 	}
 }
